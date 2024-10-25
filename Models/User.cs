e61a59584705997f384e3ef86adac099cbec9252
manoilokate <kateryna.manoilo@lnu.edu.ua>
2024-10-25 12:20:37 +0300

    connection with postgers

---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonalRecords.Models
+{
+    public class User
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(25)]
+        public string UserName { get; set; }
+        [Required]
+        [StringLength(25)]
+        public string Password { get; set; }
+    }
+}
