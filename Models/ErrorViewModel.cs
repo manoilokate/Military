59d1fa5d032642e4d1cca11d40eb9b68b1585a06
manoilokate <kateryna.manoilo@lnu.edu.ua>
2024-10-25 11:54:58 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PersonalRecords.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
